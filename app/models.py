5bd085b318c253780bcbfb3610d38ba07ffd03c9
sohaibfarooqi <sohaibfarooqi@hotmail.com>
2016-10-18 20:26:59 +0500

    materialized path model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from .extentions import db
 from sqlalchemy import func
+from sqlalchemy_utils import LtreeType
 
 class Entity:
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +13,21 @@ class FirstModel(db.Model,Entity,TimestampMixin):
     parent_id = db.Column(db.Integer, index=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+    def __init__(self,id,created_at,updated_at,parent_id,title,description):
+        self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.parent_id = parent_id
+        self.title = title
+        self.description = description
+
+
+class SecondModel(db.Model,Entity,TimestampMixin):
+    parent_id = db.Column(db.Integer, index=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    path = db.Column(LtreeType)
 
     def __init__(self,id,created_at,updated_at,parent_id,title,description):
         self.id = id
